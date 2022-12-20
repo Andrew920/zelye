40d36f901029ecb22d6a8ed4bfd90561a58c2e8f
Andrew <andrew>
2022-12-20 12:45:59 -0500

    update

--- code/api/services/db.js
+++ code/api/services/db.js
@@ -6,17 +6,12 @@ const conn = mysql.createConnection({
     database: "zelye"
 });
 
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-sleep(20000).then(() => {
-    conn.connect((err) => {
-        if(err){
-            console.log('Error connecting to Db ' + err);
-            return;
-          }
-          console.log('Connection established');
-    });
+conn.connect((err) => {
+    if(err){
+        console.log('Error connecting to Db ' + err);
+        return;
+      }
+      console.log('Connection established');
 });
 
 module.exports = conn;
