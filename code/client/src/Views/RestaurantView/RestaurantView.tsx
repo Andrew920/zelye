af0a146e13dd493dda483dffe3233d0920ec3f1b
David Bešter <besterdavid01@gmail.com>
2022-12-28 17:28:46 +0100

    frontend: fix menu item list

--- code/client/src/Views/RestaurantView/RestaurantView.tsx
+++ code/client/src/Views/RestaurantView/RestaurantView.tsx
@@ -1,11 +1,25 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
-import { BackgroundImage, Icon, IconName, Spinner } from 'Components';
+import { BackgroundImage, Icon, IconButton, IconName, Spinner } from 'Components';
 import { useGetRestaurantQuery, useGetSponsorQuery } from 'Api';
 import { CategoryT } from 'Types';
 import { CategoryCard } from './CategoryCard';
 import { Header } from './Header/Header';
 import './RestaurantView.scss';
+
+const splitCategories = (categories: CategoryT[]): [CategoryT[], CategoryT[]] => {
+  return categories.reduce(
+    (acc, category) => {
+      if (category.col === 0) {
+        acc[0].push(category);
+      } else {
+        acc[1].push(category);
+      }
+      return acc;
+    },
+    [[], []] as [CategoryT[], CategoryT[]]
+  );
+};
 
 export const RestaurantView: FC = () => {
   const { restaurant } = useParams();
@@ -30,7 +44,7 @@ export const RestaurantView: FC = () => {
             <Icon icon={IconName.Info} />
           </div>
           <div className='restaurant-view__categories-table'>
-            {restaurantData.menu.map((categoryArr: CategoryT[], i: number) => (
+            {splitCategories(restaurantData.menu).map((categoryArr: CategoryT[], i: number) => (
               <div key={`category-${i}`} className='restaurant-view__categories_col'>
                 {categoryArr.map((category: CategoryT) => (
                   <CategoryCard
@@ -43,6 +57,14 @@ export const RestaurantView: FC = () => {
                 ))}
               </div>
             ))}
+          </div>
+          <div className='restaurant-view__action_button'>
+            <IconButton
+              variant='primary'
+              size='large'
+              icon={IconName.RateReview}
+              href='review-restaurant'
+            />
           </div>
         </div>
       ) : (
