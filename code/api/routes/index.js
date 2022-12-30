2099d2bc5c12fcb9dc4d8a9f5e144c05907de479
David Bešter <besterdavid01@gmail.com>
2022-12-30 13:53:10 +0100

    backend: add sposnor mock request

--- code/api/routes/index.js
+++ code/api/routes/index.js
@@ -7,9 +7,14 @@ const router = express.Router();
 //     res.send('hello world')
 //   })
 
-router.route('/restaurant/:id').get((req, res) => {
-    console.log('restaurant id: ' + req.params.id);
-    controllers.getRestaurant(req, res);
+router.route("/restaurant/:id").get((req, res) => {
+  console.log("restaurant id: " + req.params.id);
+  controllers.getRestaurant(req, res);
+});
+
+router.route("/sponsor").get((req, res) => {
+  console.log("sponsor " + req.params.id);
+  res.json({ id: 1, name: "coca-cola", logotype: null, background: null });
 });
 
 module.exports = router;
