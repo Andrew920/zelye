5d2a92a7830d26033606a4e6e20fe19536b777fc
David Bešter <besterdavid01@gmail.com>
2023-01-06 22:43:19 +0100

    frontend: fix api call

--- code/client/src/Api/config.ts
+++ code/client/src/Api/config.ts
@@ -3,7 +3,8 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query';
 const { REACT_APP_SERVER_URL, NODE_ENV } = process.env;
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: NODE_ENV == 'development' ? REACT_APP_SERVER_URL + '/api/' : 'api/',
+  baseUrl:
+    NODE_ENV == 'development' ? REACT_APP_SERVER_URL + '/api/' : REACT_APP_SERVER_URL + '/api/',
   prepareHeaders: (headers: Headers) => {
     // If we have a token set in state, let's assume that we should be passing it.
     const token: string = localStorage.getItem('token') || '';
