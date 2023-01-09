6a13de88500507c9785f0f79f3ef56f0549b7d8f
David Bešter <besterdavid01@gmail.com>
2023-01-09 21:04:25 +0100

    db: change parsing data

--- code/database/jedi.py
+++ code/database/jedi.py
@@ -1,10 +1,15 @@
 
 items = []
 for x in open('food.csv').readlines():
-    name, desc, subcategory, price = x.strip().split(';')
-    items.append((name, desc, subcategory, price))
+    item = x.strip().split(';')
+    items.append((item))
 f = open('food.sql', 'w')
 
-for id, (name, desc, sub, price) in enumerate(items):
+for id, item in enumerate(items):
+    name = item[0]
+    desc = item[1]
+    sub = item[2]
+    price = item[3]
+    image = item[4] or "default.jpeg"
     f.write(
-        f"INSERT INTO item(id, name, image, description, subcategory_id, price, price_unit) VALUES ({id+1},\"{name}\",\"image-{id}.jpeg\",\"{desc}\",{int(sub) + 1},{float('.'.join(price.split(',')))},\"EUR\");\n")
+        f"INSERT INTO item(id, name, image, description, subcategory_id, price, price_unit) VALUES ({id+1},\"{name}\",\"{image}\",\"{desc}\",{int(sub) + 1},{float('.'.join(price.split(',')))},\"EUR\");\n")
