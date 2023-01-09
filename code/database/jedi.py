817968a541231a60017100904c4fd4d50ed7b996
David Bešter <besterdavid01@gmail.com>
2023-01-09 21:06:55 +0100

    db: fix

--- code/database/jedi.py
+++ code/database/jedi.py
@@ -10,6 +10,6 @@ for id, item in enumerate(items):
     desc = item[1]
     sub = item[2]
     price = item[3]
-    image = item[4] or "default.jpeg"
+    image = item[4] or "default.jpg"
     f.write(
         f"INSERT INTO item(id, name, image, description, subcategory_id, price, price_unit) VALUES ({id+1},\"{name}\",\"{image}\",\"{desc}\",{int(sub) + 1},{float('.'.join(price.split(',')))},\"EUR\");\n")
